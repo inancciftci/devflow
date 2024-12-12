ea4df557bb38077e4eaf826f58a2670a38c25524
inancciftci <enonch@hotmail.com>
2024-12-12 16:28:39 +0300

    eslint&prettier rules

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-violet-700 font-bold text-3xl">
+      <h1 className="text-3xl font-black text-white">
         Welcome to the world of Next.js
       </h1>
     </div>
