92de43f1adb48cb855abbb0aabe630ea78d69f30
inancciftci <enonch@hotmail.com>
2025-01-29 15:47:09 +0300

    Implement the UI of the Question Form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,26 @@
+import QuestionForm from "@/components/forms/QuestionForm";
+import Image from "next/image";
 import React from "react";
 
 const AskAQuestion = () => {
-  return <div>Ask a question</div>;
+  return (
+    <>
+      <div className="flex items-center gap-4">
+        <Image
+          className="invert-colors"
+          src={"/icons/question.svg"}
+          width={35}
+          height={35}
+          alt="Question Mark"
+        />
+        <h1 className="h1-bold text-dark100_light900">Ask a public question</h1>
+      </div>
+
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </>
+  );
 };
 
 export default AskAQuestion;
